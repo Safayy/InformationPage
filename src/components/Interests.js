da2caf2946996d97050eee33b4c9da149ca47d23
Safa Yousif <safa.yousif@outlook.com>
2022-07-05 19:17:37 +0800

    edit interests

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -4,7 +4,7 @@ export default function Interests() {
     return (
         <div className="section">
             <h2 className="section--heading">Interests</h2>
-            <p className="section--content">Food. Coding. Loving. Drawing. Killing. Eating. Surfing. Food. Coding. Loving. Drawing. Killing. Eating. Surfing. Food. Coding. Loving. Drawing. Killing. Eating. Surfing. </p>
+            <p className="section--content">Coding. Sewing. Learning</p>
         </div>
     )
 }
