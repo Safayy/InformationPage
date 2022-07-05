863ecd9b9644da2fd58b48365aa48002f8749ae5
Safa Yousif <safa.yousif@outlook.com>
2022-07-05 19:16:26 +0800

    edit description

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <div className="section">
             <h2 className="section--heading">About</h2>
-            <p className="section--content">I am a student who is studying to be a developer. I put effort into my internships and expect to receive respect. This is the end of the sentence. Goodluck!</p>
+            <p className="section--content">I am a student who is studying to be a website developer. Passionate about full stack website development and mobile applications, I work hard to create solutions to the problems I see.</p>
         </div>
     )
 }
